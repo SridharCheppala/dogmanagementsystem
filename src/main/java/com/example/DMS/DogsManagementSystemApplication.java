3132f80f71f2b2e31ef9e9097da87ca4cd54aa19
Sri <156224448+SridharCheppala@users.noreply.github.com>
2024-11-20 13:32:34 -0600

    dogmanagementsystem

---
+++ src/main/java/com/example/DMS/DogsManagementSystemApplication.java
@@ -0,0 +1,16 @@
+package com.example.DMS;
+
+import org.springframework.boot.SpringApplication;
+/**
+ * @author S571565
+ */
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogsManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogsManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/DMS/repository/TrainerRepository.java
@@ -0,0 +1,12 @@
+package com.example.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.DMS.Models.Trainer;
+/**
+ * @author S571565
+ */
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
